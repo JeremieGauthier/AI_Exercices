8b434bac63e60cca59cadaa91f1d791a430bd63b
Jeremie Gauthier <jeremie.gauthier.1@outlook.com>
2020-03-22 15:43:46 -0400

    Several Changes

--- Atari_2600_Breakout.py
+++ Atari_2600_Breakout.py
@@ -8,7 +8,7 @@ for _ in range(10000):
     env.render()
     action = env.action_space.sample()
     observation, reward, done, info = env.step(action)
-    time.sleep(0.03)
+     
 
 if done:
     obervaton = env.reset()
