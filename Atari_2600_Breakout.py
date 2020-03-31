9dfde7fda34457108c44dd8b6ed0579bd5d8024b
Jeremie Gauthier <jeremie.gauthier.1@outlook.com>
2020-03-31 13:03:14 -0400

    Created AtariBreakoutEnvManager

--- Atari_2600_Breakout.py
+++ Atari_2600_Breakout.py
@@ -15,7 +15,8 @@ class DQN(nn.Module):
 
         self.conv = nn.Conv2d(in_channels=1)
 
-class AtariBreakout():
+class AtariBreakoutEnvManager():
+
     def __init__(self, device):
         self.device = device
         self.env = gym.make('Breakout-v0').unwrapped
@@ -35,32 +36,49 @@ class AtariBreakout():
 
     def get_transform(self, img):
         img = np.ascontiguousarray(img, dtype=np.float32) / 255
-
-    def get_process(self):
-        img = self.render("rgb_array").transpose((2, 0, 1))
         img = torch.from_numpy(img)
 
+        convert = T.Compose([T.ToPILImage(),
+                         T.Grayscale(),
+                         T.Resize((110, 84)),
+                         T.ToTensor()])  
+        
+        return convert(img)
+
+    def get_crop(self, img):
+        top = 17
+        bottom = 101
+        img = img[:, top:bottom, :]
+        
+        return img
+
+    def get_process(self):
+        img = self.env.render("rgb_array").transpose((2, 0, 1))
+        img = self.get_transform(img) #Reshape to (110, 84) and RGB to GrayScale
+        img = self.get_crop(img) # Crop top and bottom to obtain shape (84, 84)
+
+        return img.unsqueeze(dim=0)
+
     def get_height(self):
-        screen = self.get_processed()
-        return img.shape[2]
-    
+        img = self.get_process()
+        return img.shape[2] 
+
     def get_width(self):
-        img = self.get_processed_screen()
-        return img.shape[3]
-
-
+        img = self.get_process()
+        return img.shape[3] 
 
 if __name__ == "__main__":
 
-    env = gym.make('Breakout-v0')
+    
     nb_games = 1000
     done = False
     
     for i in range(nb_games):
+        env = gym.make("Breakout-v0")
         env.reset()
         
         while not done:
-            env.render(mode="human")
+            # env.render(mode="human")
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
             import ipdb; ipdb.set_trace()
