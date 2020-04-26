e496f2dbcb4c3a8d19cec76fd33cb5607f82481a
Jeremie Gauthier <jeremie.gauthier.1@outlook.com>
2020-04-26 15:10:12 -0400

    Changes

---
+++ CartPole/CartPole_with_A2C/CartPole_with_A2C.py
@@ -0,0 +1,28 @@
+from actor_critic_discrete import Agent
+
+from gym import wrappers
+from torch.utils.tensorboard import SummaryWriter
+
+import numpy as np 
+import gym
+
+if __name__ == "__main__":
+    agent = Agent(alpha = 0.00001, beta=0.0005, input_dims=[4], n_actions=2,
+                l1_size=32, l2_size=32)
+
+    env = gym.make("CartPole-v1")
+    writer = SummaryWriter("run")
+
+    num_episodes = 2500
+    for step in range(num_episodes):
+        score=0
+        done=False
+        observation = env.reset()
+        while not done:
+            action = agent.choose_action(observation)
+            observation_, reward, done, _ = env.step(action)
+            score += reward
+            agent.learn(observation, reward, observation_, done)
+            observation = observation_
+        writer.add_scalar("score", score, step) 
+        print("episode :%d, score :%.3f" % (step, score))
---
+++ CartPole/CartPole_with_A2C/actor_critic_discrete.py
@@ -0,0 +1,62 @@
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import torch as T
+import numpy as np 
+
+class GenericNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(GenericNetwork, self).__init__()
+        self.lr = lr
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+    def forward(self, observation):
+        state = T.Tensor(observation).to(self.device)
+        output = F.relu(self.fc1(state))
+        output = F.relu(self.fc2(output))
+        output = self.fc3(output)
+        return output
+
+
+class Agent():
+    def __init__(self, alpha, beta, input_dims, gamma=0.99, 
+                l1_size=256, l2_size=256, n_actions=2):
+        self.gamma = gamma
+        self.log_probs = None
+        self.actor = GenericNetwork(alpha, input_dims, l1_size, l2_size, 
+                                    n_actions)
+        self.critic = GenericNetwork(beta, input_dims, l1_size, l2_size, 
+                                    n_actions=1)
+        
+    def choose_action(self, observation):
+        probabilities  = F.softmax(self.actor.forward(observation))
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        self.log_probs = action_probs.log_prob(action)
+
+        return action.item()
+    
+    def learn(self, state, reward, new_state, done):
+        self.actor.optimizer.zero_grad()
+        self.critic.optimizer.zero_grad()
+
+        critic_value = self.critic.forward(state)
+        critic_value_ = self.critic.forward(new_state)
+
+        delta = ((reward + self.gamma * critic_value_ * (1 - int(done))) - critic_value)
+
+        actor_loss = -1*self.log_probs * delta
+        critic_loss = delta**2
+
+        (actor_loss + critic_loss).backward()
+
+        self.actor.optimizer.step()
+        self.critic.optimizer.step()
